0f711376f3a2699cb0e257f3048e05817e561e23
Guest User <Guest@epicodus-26.local>
2018-07-03 13:14:54 -0700

    add ModelTests folder into Calendar.Tests,  then add LeapYearTest.cs into the newly created ModelTests folder

--- Calendar.Tests/ModelTests/LeapYearTests.cs
+++ Calendar.Tests/ModelTests/LeapYearTests.cs
@@ -3,3 +3,23 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 //imports the Calendar namespace from the Calendar project for use in our tests
 using Calendar;
+
+namespace Calendar.Tests
+{
+
+  [TestClass] //This tells MSTest that the following class contains tests to be run
+  public class LeapYearTest
+  {
+    //This tells the compiler that the following code block is an individual
+    //test that should be run by MSTest
+    [TestMethod]
+
+    //This is the test.
+    //naming convention is basically
+    //MethodToBeTested_BehaviorToBeTested_ExpectedResultOfTest
+    public void IsLeapYear_NumberDivisibleByFour_True()
+    {
+      // eventually your testing code will go here
+    }
+  }
+}
