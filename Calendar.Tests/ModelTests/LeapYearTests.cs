fb2379a1a47ffcd66e7ff401719b59f14eea333e
Guest User <Guest@epicodus-26.local>
2018-07-03 13:13:52 -0700

    add ModelTests folder into Calendar.Tests,  then add LeapYearTest.cs into the newly created ModelTests folder

---
+++ Calendar.Tests/ModelTests/LeapYearTests.cs
@@ -0,0 +1,5 @@
+//imports the MSTest framework for us to use
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+//imports the Calendar namespace from the Calendar project for use in our tests
+using Calendar;
